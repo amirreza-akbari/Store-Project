b266692da458dbde00e8d63b2a1ffde632d1b127
amirreza-akabri <amirrezaakbari8402@gmailcom>
2024-08-30 21:03:25 -0700

    delete for files

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('nav ul').classList.toggle('menu-open');
-    document.querySelector('header').classList.toggle('menu-open');
-});
